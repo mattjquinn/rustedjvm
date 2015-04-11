9cdb5848b7c7f41656fcf05d1183b96508a4fa44
Matt Quinn <matt@mattjquinn.com>
2015-04-10 23:40:38 -0700

    Cleaning up; using vector rather than string to read file input from classfile.

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,19 @@ use std::io::prelude::*;
 use std::path::Path;
 
 fn main() {
-    // Create a path to the desired file
-    let path = Path::new("HelloWorld.java");
+    let path = Path::new("HelloWorld.class");
     let display = path.display();
 
-    // Open teh path in read-only mode, returns `io::Result<File>`
     let mut file = match File::open(&path) {
-        // The `description` method of `io::Error` returns a string that
-        // describes the error
         Err(why) => panic!("couldn't open {}: {}", display,
-                                                   Error::description(&why)),
+                Error::description(&why)),
         Ok(file) => file,
     };
 
-    // Read the file contents into a string, returns `io::Result<usize>`
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
+    let mut v = Vec::new();
+    match file.read_to_end(&mut v) {
         Err(why) => panic!("couldn't read {}: {}", display,
-                                                   Error::description(&why)),
-        Ok(_) => print!("{} contains:\n{}", display, s),
+                Error::description(&why)),
+        Ok(_) => print!("{} contains:\n{}\n", display, v.len()),
     }
-
-    // `file` goes out of scope, and the "hello.txt" file gets closed
 }
