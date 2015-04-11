c3e4f3bb3eaa6e49cce827fd0ab84b561e8c3285
Matt Quinn <matt@mattjquinn.com>
2015-04-10 23:47:51 -0700

    Able to display bytecodes, one per line.

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,19 @@ fn main() {
     let display = path.display();
 
     let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display,
-                Error::description(&why)),
+        Err(why) => panic!("[ERROR] Unable to open {}: {}",
+                display, Error::description(&why)),
         Ok(file) => file,
     };
 
     let mut v = Vec::new();
     match file.read_to_end(&mut v) {
-        Err(why) => panic!("couldn't read {}: {}", display,
-                Error::description(&why)),
+        Err(why) => panic!("[ERROR] Unable to read {}: {}",
+                display, Error::description(&why)),
         Ok(_) => print!("{} contains:\n{}\n", display, v.len()),
+    }
+
+    for x in v.iter() {
+        println!("{:x}", x);
     }
 }
