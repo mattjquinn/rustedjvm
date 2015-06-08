9c14af09a579a5900e6e6657be0c03fb507375ab
Matt Quinn <matt@mattjquinn.com>
2015-06-07 17:01:57 -0700

    Implementing the first half of invokespecial.

--- src/rustedjvm/interpreter.rs
+++ src/rustedjvm/interpreter.rs
@@ -40,9 +40,20 @@ pub fn run(class: Class) {
     /*
      * Begin executing method bytecodes.
      */
-    for bytecode in code_attr.code_slice {
-        match *bytecode {
-            0x2a => aload_0(&local_var_arr, &mut operand_stack),
+    println!("Interpreting...");
+    let mut bytecode_idx: usize = 0;
+    while bytecode_idx < code_attr.code_slice.len() {
+        match code_attr.code_slice[bytecode_idx] {
+            0x2a => {
+                aload_0(&local_var_arr, &mut operand_stack);
+                bytecode_idx += 1;
+            },
+            0xb7 => {
+                invokespecial(&mut operand_stack,
+                              code_attr.code_slice[bytecode_idx+1],
+                              code_attr.code_slice[bytecode_idx+2]);
+                bytecode_idx += 3;
+            },
             unsup_code => panic!("[ERROR] Encountered unsupported \
                                   bytecode: {:x}", unsup_code),
         }
@@ -51,5 +62,18 @@ pub fn run(class: Class) {
 
 fn aload_0<'a, 'b>(local_var_arr: &'b Vec<Object<'a>>,
                operand_stack: &mut Vec<&'b Object<'a>>) {
+    println!("aload_0");
     operand_stack.push(&local_var_arr[0]);
+}
+
+fn invokespecial(operand_stack: &mut Vec<&Object>,
+                 indexbyte1: u8,
+                 indexbyte2: u8) {
+    println!("invokespecial");
+    let object_ref: &Object = match operand_stack.pop() {
+        Some(e) => e,
+        None => panic!("[ERROR] Expected objectref, found None."),
+    };
+    println!("indexbyte1: {}", indexbyte1);
+    println!("indexbyte2: {}", indexbyte2);
 }
